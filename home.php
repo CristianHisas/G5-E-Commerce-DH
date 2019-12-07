1ee23ee4503338bc65d33068334a91f6de5aaba8
12jmad <55392943+12jmad@users.noreply.github.com>
2019-12-06 21:23:21 -0300

    update home

--- home.php
+++ home.php
@@ -1,3 +1,15 @@
+<?php
+session_start();
+$pagina="Home";
+include_once("includes/funciones.php");
+include_once("includes/baseDeDatos.php");
+if(isset($_SESSION["activeUser"]) && !is_null($_SESSION["activeUser"])){
+    if($_SESSION["activeUser"]["fotoPerfil"]==""){
+      $_SESSION["activeUser"]["fotoPerfil"]=(isset($imagenUsuario))?$imagenUsuario:"img/perfil.png";
+    }  
+    $activeUser=$_SESSION["activeUser"];
+  }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,9 +24,13 @@
     <link rel="shortcut icon" href="favicon.ico"/>
 </head>
 <body class="main-perfil">
-    <!--Comienza el header-->
-    <?php include_once "includes/header.php"; ?>
-    <?php include_once "includes/modal.php"; ?>
+        <!--Comienza el header-->
+        <header class="container-fluir fixed-top">
+    <?php
+      include_once("includes/headerPerfil.php");
+    ?>
+    </header>
+     <!--Fin el header-->
 
     <main>
         <div class="inner-main" id="home">
@@ -71,7 +87,7 @@
                         Vehículos
                     </div>
                 </div>
-                <a href="lista-productos.html" class="irDescripcion"><!--Saque el onclick por que pertenece a javascript--->
+                <a href="lista-productos.php" class="irDescripcion"><!--Saque el onclick por que pertenece a javascript--->
                 <div class="categoria">
                     <div class="icono">
                         <img src="img/categorias/tel.png" alt="">
@@ -138,39 +154,19 @@
 
 
       <!-- MainBody End ============================= -->
-  <!-- Footer ================================================================== -->
-  <footer>
-      <div class="container-fluid bg-dark px-auto py-4 footer-cambiado ">
-        <div class="row d-flex justify-content-between">
-            <div class="col-12 col-sm-4 col-md-4 col-lg-4 col-xd-4">
-                <h5>ACCOUNT</h5>
-                <a href="login.html" class="">YOUR ACCOUNT</a>
-                <a href="login.html">PERSONAL INFORMATION</a>
-                <a href="login.html">ADDRESSES</a>
-                <a href="login.html">DISCOUNT</a>
-                <a href="login.html">ORDER HISTORY</a>
-              </div>
-              <div class="col-12 col-sm-4 col-md-4 col-lg-4 col-xd-4">
-                <h5>INFORMATION</h5>
-                <a href="contacto.html">CONTACT</a>
-                <a href="registro.php">REGISTRATION</a>
-                <a href="legal_notice.html">LEGAL NOTICE</a>
-                <a href="tac.html">TERMS AND CONDITIONS</a>
-                <a href="faq.html">FAQ</a>
-              </div>
-              <div id="" class="col-12 col-sm-4 col-md-4 col-lg-4 col-xd-4 social text-center">
-                <h5 class="social">SOCIAL MEDIA </h5>
-                <a href="#" class="social"><img width="60" height="60" src="https://live.staticflickr.com/65535/49093342407_45310a774c_o.png" title="facebook" alt=""/></a>
-                <a href="#" class="social"><img width="60" height="60" src="https://live.staticflickr.com/65535/49092642883_c0782ddd4d_o.png" title="twitter" alt=""/></a>
-                <a href="#" class="social"><img width="60" height="60" src="https://live.staticflickr.com/65535/49092641488_040001d127_o.png" title="youtube" alt=""/></a>
-              </div>
-            </div>
-        <p class="mx-auto text-center my-2 py-2">Copyright &copy; Your Website 2019</p>
-      </div><!-- Container End -->
-    </footer>
-    <!-- Footer End================================================================== -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<!-- footer -->
+<?php
+    include_once("includes/footer.php");
+  ?>
+<!-- fin footer -->
+<!-- Modal -->
+  <?php 
+    include_once("includes/modal.php");  
+  ?>                          
+<!-- Fin modal -->
+  <?php
+    include_once("includes/scriptBootstrap.php");
+  ?>
+    <script src="https://kit.fontawesome.com/67f61afa3e.js" crossorigin="anonymous"></script>
 </body>
 </html>
