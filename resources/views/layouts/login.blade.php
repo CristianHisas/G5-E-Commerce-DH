2fc01243d5a140df14287fd39ad58800cca4c557
12jmad <55392943+12jmad@users.noreply.github.com>
2020-02-17 16:04:32 -0300

    taran

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -327,15 +327,6 @@ foreach ($pagina as $key => $value) {
   </div>
 </div>
 <!-- Fin modal -->
-<<<<<<< HEAD
-=======
-
-<!-- Fin modal -->
-<!-- Scripts ============================================= -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
->>>>>>> a202230a9b555f5596d69f3cac036d9a1f1d1dd0
 
 <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 </body>
